aea659b04716c9ffe7c6306625e099be0f0bf378
tuyen_kun <20130459@st.hcmuaf.edu.vn>
2023-07-24 16:32:35 +0700

    tạo function để thêm dữ liệu vào bảng order_details

--- src/main/java/hibernate/Main.java
+++ src/main/java/hibernate/Main.java
@@ -10,24 +10,24 @@ public class Main {
         long startTime = System.currentTimeMillis();
         Session session = null;
         try {
-//            session = ConfigHibernate.getFactory().openSession();
-//            System.out.println("Kết nối đến database thành công");
-//
-//            DataStatusProduct.addDataToTable_StatusProducts();
-//            DataStatusOrder.addDataToTable_StatusOrders();
-//            DataBrand.addDataToTable_Brands();
-//            DataSize.addDataToTable_Sizes();
-//            DataPriceRange.addDataToTable_PriceRanges();
-//            DataTypeProduct.addDataToTable_TypeProducts();
-//
-//            for (int i = 0; i < 100; i++) {
-//                DataProduct.addDataToTable_Products();
-//            }
-//
-//            DataImageProduct.addDataToTable_ImageProducts();
-//            DataSizeProduct.addDataToTable_SizeProducts();
-//            DataHistoryPriceProduct.addDataToTable_HistoryPriceProducts();
-//            DataOrder.addDataToTable_Orders();
+            session = ConfigHibernate.getFactory().openSession();
+            System.out.println("Kết nối đến database thành công");
+
+            DataStatusProduct.addDataToTable_StatusProducts();
+            DataStatusOrder.addDataToTable_StatusOrders();
+            DataBrand.addDataToTable_Brands();
+            DataSize.addDataToTable_Sizes();
+            DataPriceRange.addDataToTable_PriceRanges();
+            DataTypeProduct.addDataToTable_TypeProducts();
+
+            for (int i = 0; i < 100; i++) {
+                DataProduct.addDataToTable_Products();
+            }
+
+            DataImageProduct.addDataToTable_ImageProducts();
+            DataSizeProduct.addDataToTable_SizeProducts();
+            DataHistoryPriceProduct.addDataToTable_HistoryPriceProducts();
+            DataOrder.addDataToTable_Orders();
             DataOrderDetail.addDataToTable_OrderDetails();
 
             long endTime = System.currentTimeMillis();
